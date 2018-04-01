1ebaf57b0bdcaf9820d9c653e663634b8c8c2f81
Alec Jacobs <jacobs.alec@gmail.com>
2018-04-01 05:44:57 -0400

    move src alias into bash function, see http://www.joshstaiger.org/archives/2005/07/bash_profile_vs.html

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,5 @@
 # my aliases
 alias ll="ls -laG" # duh
-alias src="source ~/.bashrc" # re source everything?
 alias wk="cd $CURRENT_PROJECT_DIRECTORY" # goto current working directory
 alias swk="pwd > ~/.current-project && src" # set current working directory
 alias stw="bundle exec sidekiq -c 1"
@@ -17,3 +16,16 @@ alias amp="bash ~/workspace/build-meteor-app/build-app" # setup blank meteor app
 alias weather="coffee ~/workspace/weather-me/run.coffee" # shortcut for the weather
 alias mou="open /Applications/Mou.app" # Mac OS Yoesmite broke this?
 alias cnm="meteor --port $IP:$PORT"
+
+src() {
+  if [ -f ~/.bashrc ]; then
+    source ~/.bashrc
+    echo "sourcing .bashrc"
+  fi
+
+  if [ -f ~/.bash_profile ]; then
+    source ~/.bash_profile
+    echo "sourcing .bash_profile"
+  fi
+}
+
